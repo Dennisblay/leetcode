e075fccc1aed999e4889967fb3a8e0f257cdf0ea
Dennis <dennisboachie9@gmail.com>
2024-09-30 15:38:22 +0000

    find middle node

--- ds/ll/exercise.py
+++ ds/ll/exercise.py
@@ -121,6 +121,17 @@ class LinkedList:
         temp.next = None
         self.length -= 1
 
+    def find_middle_node(self) -> Optional['Node']:
+        if self.head is None:
+            return None
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+
 
 if __name__ == "__main__":
     ll = LinkedList(0)
--- ds/ll/ll_tut.py
+++ ds/ll/ll_tut.py
@@ -132,6 +132,17 @@ class LinkedList:
             before = current
             current = after
 
+    def find_middle_node(self) -> Optional['Node']:
+        if self.head is None:
+            return None
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+
 
 if __name__ == "__main__":
     ll = LinkedList(0)
